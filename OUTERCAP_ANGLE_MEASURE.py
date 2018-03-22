e9076c90452a4c90e2af4fd8e4c25c95bb7dc788
Nate Evans <evans@ad.visiongate.com>
2018-03-22 10:26:58 -0700

    fixed angle_calc.txt naming issue

--- OUTERCAP_ANGLE_MEASURE.py
+++ OUTERCAP_ANGLE_MEASURE.py
@@ -57,7 +57,7 @@ def main(example_path):
     vals['angle_x'] = np.degrees(np.arctan( ( vals['upper ledge']['DX-in'] - vals['lower ledge']['DX-in'] ) / LEDGE_SEPARATION_DISTANCE)) 
     vals['angle_y'] = np.degrees(np.arctan( ( vals['upper ledge']['DY-in'] - vals['lower ledge']['DY-in'] ) / LEDGE_SEPARATION_DISTANCE)) 
 
-    f = open(example_path+'\\angle_calc-' + example_path.split('/')[-1] +'.txt', 'w')
+    f = open(example_path+'\\angle_calc-' + example_path.split('/')[-1].replace('.','') +'.txt', 'w')
     f.write(str(vals))
     try:
         f.write('\n\n\n\n\n')
